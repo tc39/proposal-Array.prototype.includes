0aed5b9145f07bf7e330724de32242f17d974399
Domenic Denicola <domenic@domenicdenicola.com>
2014-08-04 16:48:55 -0400

    Add zero-length array/array-like tests

---
+++ test/Array.prototype.contains_zero-length.js
@@ -0,0 +1,57 @@
+// Copyright (C) 2014 Domenic Denicola. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: Array.prototype.contains should always return false on zero-length objects
+author: Domenic Denicola
+---*/
+
+if ([].contains(2) !== false) {
+    $ERROR('Expected [] to not contain 2');
+}
+
+if ([].contains() !== false) {
+    $ERROR('Expected [] to not contain (no argument passed)');
+}
+
+if ([].contains(undefined) !== false) {
+    $ERROR('Expected [] to not contain undefined');
+}
+
+if ([].contains(NaN) !== false) {
+    $ERROR('Expected [] to not contain NaN');
+}
+
+
+if (Array.prototype.contains.call({ length: 0 }, 2) !== false) {
+    $ERROR('Expected { length: 0 } to not contain 2');
+}
+
+if (Array.prototype.contains.call({ length: 0 }) !== false) {
+    $ERROR('Expected { length: 0 } to not contain (no argument passed)');
+}
+
+if (Array.prototype.contains.call({ length: 0 }, undefined) !== false) {
+    $ERROR('Expected { length: 0 } to not contain undefined');
+}
+
+if (Array.prototype.contains.call({ length: 0 }, NaN) !== false) {
+    $ERROR('Expected { length: 0 } to not contain NaN');
+}
+
+
+if (Array.prototype.contains.call({ length: 0, 0: 2 }, 2) !== false) {
+    $ERROR('Expected { length: 0, 0: 2 } to not contain 2');
+}
+
+if (Array.prototype.contains.call({ length: 0, 0: undefined }) !== false) {
+    $ERROR('Expected { length: 0, 0: undefined } to not contain (no argument passed)');
+}
+
+if (Array.prototype.contains.call({ length: 0, 0: undefined }, undefined) !== false) {
+    $ERROR('Expected { length: 0, 0: undefined } to not contain undefined');
+}
+
+if (Array.prototype.contains.call({ length: 0, 0: NaN }, NaN) !== false) {
+    $ERROR('Expected { length: 0, 0: NaN } to not contain NaN');
+}
