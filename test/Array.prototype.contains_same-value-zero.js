4f96d9b72129dbed2aedca52d814ae5aaf046129
Domenic Denicola <domenic@domenicdenicola.com>
2014-08-08 18:36:34 -0400

    Add SameValueZero tests; remove illustrative example tests

---
+++ test/Array.prototype.contains_same-value-zero.js
@@ -0,0 +1,51 @@
+// Copyright (C) 2014 Domenic Denicola. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: Array.prototype.contains should use the SameValueZero algorithm to compare
+author: Domenic Denicola
+---*/
+
+if ([1, 2, 3].contains(2) !== true) {
+    $ERROR('Expected [1, 2, 3] to contain 2');
+}
+
+if ([1, 2, 3].contains(4) !== false) {
+    $ERROR('Expected [1, 2, 3] to not contain 4');
+}
+
+if ([1, 2, NaN].contains(NaN) !== true) {
+    $ERROR('Expected [1, 2, NaN] to contain NaN');
+}
+
+if ([1, 2, -0].contains(+0) !== true) {
+    $ERROR('Expected [1, 2, -0] to contain +0');
+}
+
+if ([1, 2, -0].contains(-0) !== true) {
+    $ERROR('Expected [1, 2, -0] to contain -0');
+}
+
+if ([1, 2, +0].contains(-0) !== true) {
+    $ERROR('Expected [1, 2, +0] to contain -0');
+}
+
+if ([1, 2, +0].contains(+0) !== true) {
+    $ERROR('Expected [1, 2, +0] to contain +0');
+}
+
+if ([1, 2, -Infinity].contains(+Infinity) !== false) {
+    $ERROR('Expected [1, 2, -Infinity] to not contain +Infinity');
+}
+
+if ([1, 2, -Infinity].contains(-Infinity) !== true) {
+    $ERROR('Expected [1, 2, -Infinity] to contain -Infinity');
+}
+
+if ([1, 2, +Infinity].contains(-Infinity) !== false) {
+    $ERROR('Expected [1, 2, +Infinity] to not contain -Infinity');
+}
+
+if ([1, 2, +Infinity].contains(+Infinity) !== true) {
+    $ERROR('Expected [1, 2, +Infinity] to contain +Infinity');
+}
