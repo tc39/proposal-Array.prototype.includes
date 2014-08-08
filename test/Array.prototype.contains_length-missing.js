fab151a5e896628fe99a1922d060dc66ca19f9f3
Domenic Denicola <domenic@domenicdenicola.com>
2014-08-08 19:36:23 -0400

    Add and rename basic length tests

---
+++ test/Array.prototype.contains_length-missing.js
@@ -0,0 +1,20 @@
+// Copyright (C) 2014 Domenic Denicola. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: Array.prototype.contains treats a missing length property as zero
+author: Domenic Denicola
+---*/
+
+var arrayLikeWithTraps = {
+    get 0() {
+        $ERROR('Getter for 0 was called');
+    },
+    get 1() {
+        $ERROR('Getter for 1 was called');
+    }
+};
+
+if (Array.prototype.contains.call(arrayLikeWithTraps, 'a') !== false) {
+    $ERROR('Expected a length-less object not to contain anything');
+}
